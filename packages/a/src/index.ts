f0d2c01419d6b7ecf2fb358409ded77d1e3f8da9
Michael Hunter <elec.mike@gmail.com>
2024-10-01 07:59:04 +1000

    Initial commit

---
+++ packages/a/src/index.ts
@@ -0,0 +1,2 @@
+// Package "a" is for the browser, so setTimeout returns a number
+const timeout: number = setTimeout(() => {}, 1000);
---
+++ packages/b/src/index.ts
@@ -0,0 +1,2 @@
+// Package "b" is for Node.js, so setTimeout returns a NodeJS.Timeout
+const timeout: NodeJS.Timeout = setTimeout(() => {}, 1000);
